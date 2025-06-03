7c26242d4349860b46de71e4bef9a8ef74f8bacd
Sadeepa Bandara <84839888+sadeepabandara@users.noreply.github.com>
2025-06-03 15:00:27 +1000

    feat: integrate New Relic monitoring

---
+++ newrelic.js
@@ -0,0 +1,8 @@
+'use strict';
+exports.config = {
+    app_name: ['jenkins-authify-api'],
+    license_key: '04fc1afb90d5de68fdcd61b96ea9a932FFFFNRAL',
+    logging: {
+        level: 'info',
+    },
+};
